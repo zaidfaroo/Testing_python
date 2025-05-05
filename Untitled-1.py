421696c8cb20d85b3222b3795ffe731965570045
Zaid Bin Farooq <zfarooq@corp.zscaler.com>
2025-05-05 21:49:58 +0530

    this is the beginning of the Python

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+sting = "welcome to the world"
+
+list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+new_dictionary = {
+    "name": "Zaid",
+    "age": 29,
+    "residence" : "Srinagar",
+    "Country": "India",
+    "Occupation": "Engineer",
+    "Likings": "Seeking Distraction to fill the VOID",
+    "Hobbies": "[Reading, Writing, Technology, Security]"
+
+}
+
+print(new_dictionary)
+
+hobies = new_dictionary["Hobbies"]
+print(hobies)
